faaaca76d41d374d1368309512b9452e1aa7a219
Thomas <thomas.gugatschka@gmail.com>
2021-11-17 21:03:06 +0100

    commit test

--- MainWindow.cs
+++ MainWindow.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Equin.ApplicationFramework;
+//using Equin.ApplicationFramework;
 using SyncedLogCompare.log;
 
 namespace SyncedLogCompare
